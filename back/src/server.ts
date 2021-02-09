148571b577888310124360d16b2b6e0d48824d71
Ersikan <julien.philippon@epitech.eu>
2021-02-09 21:13:54 +0100

    Add route to get address informations

--- back/src/server.ts
+++ back/src/server.ts
@@ -1,16 +1,34 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import axios from "axios";
 
-const app = express()
+const app = express();
 
 // Enable CORS for *
-app.use(cors())
+app.use(cors());
 
-app.get('/ping', (_, res) => {
-  res.send('pong')
-})
+app.get("/ping", (_, res) => {
+  res.send("pong");
+});
 
-const port = 8080
+app.get("/address/:address", (req, res) => {
+  let bitcoin_address = req.params.address;
+  axios
+    .get(`https://blockchain.info/rawaddr/${bitcoin_address}`)
+    .then((result) => {
+      res.send(result.data);
+    })
+    .catch((err) => {
+      res.status(500);
+      res.send({ error: err.message });
+    });
+});
+
+const port = 8080;
 app.listen(port, () => {
-  console.log(`Server listening on port ${port} (${process.env.NODE_ENV ?? 'unknown environment'})`)
-})
+  console.log(
+    `Server listening on port ${port} (${
+      process.env.NODE_ENV ?? "unknown environment"
+    })`
+  );
+});
